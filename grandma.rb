e2074acc16ca7c3a8acde3d198d524617c348ef0
Herman <hrivas5726@aol.com>
2019-01-15 22:47:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-  if speak == speak.upcase
+  if speak != speak.upcase
     "I LOVE YOU TOO PUMPKIN!"
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
